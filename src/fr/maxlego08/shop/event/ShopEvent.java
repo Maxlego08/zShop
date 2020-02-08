3f505549f57acd63fe436b9f849d382fa8311b15
Maxlego08 <32517160+Maxlego08@users.noreply.github.com>
2020-02-08 11:51:10 +0100

    Update ShopEvent

--- src/fr/maxlego08/shop/event/ShopEvent.java
+++ src/fr/maxlego08/shop/event/ShopEvent.java
@@ -4,7 +4,7 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-public class ShopEvent extends Event implements Cancellable{
+public class ShopEvent extends Event implements Cancellable {
 
 	private final static HandlerList handlers = new HandlerList();
 	private boolean cancelled;
@@ -23,11 +23,16 @@ public class ShopEvent extends Event implements Cancellable{
 		return handlers;
 	}
 
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+
 	/**
-	 * @param cancelled the cancelled to set
+	 * @param cancelled
+	 *            the cancelled to set
 	 */
 	public void setCancelled(boolean cancelled) {
 		this.cancelled = cancelled;
 	}
-	
+
 }
