bd03e803916844ba435c4da985ca607b0cd0f3c8
Maxlego08 <maxlego08@sfr.fr>
2023-10-02 15:28:50 +0200

    :tada: StartV3

--- src/fr/maxlego08/shop/api/button/buttons/InventoryButton.java
+++
@@ -1,9 +0,0 @@
-package fr.maxlego08.shop.api.button.buttons;
-
-import fr.maxlego08.shop.api.inventory.Inventory;
-
-public interface InventoryButton extends PermissibleButton{
-
-	public Inventory getInventory();
-	
-}
--- src/fr/maxlego08/shop/api/button/buttons/NextButton.java
+++
@@ -1,5 +0,0 @@
-package fr.maxlego08.shop.api.button.buttons;
-
-public interface NextButton extends PlaceholderButton, MoveButton {
-
-}
