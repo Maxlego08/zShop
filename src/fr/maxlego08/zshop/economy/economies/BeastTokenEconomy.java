16d9ce3fbf7d6a81e7cc79ea195e64c4ce027f02
Maxlego08 <maxlego08@sfr.fr>
2024-04-25 20:11:44 +0200

    :memo: Fix beasttoken

--- src/fr/maxlego08/zshop/economy/economies/BeastTokenEconomy.java
+++ src/fr/maxlego08/zshop/economy/economies/BeastTokenEconomy.java
@@ -1,32 +1,33 @@
 package fr.maxlego08.zshop.economy.economies;
 
-import org.bukkit.OfflinePlayer;
-
 import me.mraxetv.beasttokens.api.BeastTokensAPI;
+import org.bukkit.OfflinePlayer;
 
 public class BeastTokenEconomy extends DefaultEconomy {
 
-	/**
-	 * @param name
-	 * @param currency
-	 * @param denyMessage
-	 */
-	public BeastTokenEconomy(String name, String currency, String denyMessage) {
-		super(name, currency, denyMessage);
-	}
+    /**
+     * @param name
+     * @param currency
+     * @param denyMessage
+     */
+    public BeastTokenEconomy(String name, String currency, String denyMessage) {
+        super(name, currency, denyMessage);
+    }
 
-	@Override
-	public double getMoney(OfflinePlayer offlinePlayer) {
-		return (long) BeastTokensAPI.getTokensManager().getTokens(offlinePlayer);
-	}
+    @Override
+    public double getMoney(OfflinePlayer offlinePlayer) {
+        return offlinePlayer.isOnline() ? BeastTokensAPI.getTokensManager().getTokens(offlinePlayer.getPlayer()) : BeastTokensAPI.getTokensManager().getTokens(offlinePlayer);
+    }
 
-	@Override
-	public void depositMoney(OfflinePlayer offlinePlayer, double value) {
-		BeastTokensAPI.getTokensManager().addTokens(offlinePlayer, value);
-	}
+    @Override
+    public void depositMoney(OfflinePlayer offlinePlayer, double value) {
+		if (offlinePlayer.isOnline()) BeastTokensAPI.getTokensManager().addTokens(offlinePlayer.getPlayer(), value);
+		else BeastTokensAPI.getTokensManager().addTokens(offlinePlayer, value);
+    }
 
-	@Override
-	public void withdrawMoney(OfflinePlayer offlinePlayer, double value) {
-		BeastTokensAPI.getTokensManager().removeTokens(offlinePlayer, value);
-	}
+    @Override
+    public void withdrawMoney(OfflinePlayer offlinePlayer, double value) {
+		if (offlinePlayer.isOnline()) BeastTokensAPI.getTokensManager().removeTokens(offlinePlayer.getPlayer(), value);
+		else BeastTokensAPI.getTokensManager().removeTokens(offlinePlayer, value);
+    }
 }
